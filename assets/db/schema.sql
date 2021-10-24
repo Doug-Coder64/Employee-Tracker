7dfa0ba92a28c48a967f0e80b1d00b76aebce50c
Doug Peterson <douglaspeterson6@gmail.com>
2021-10-24 17:11:02 -0500

    Added department and roles table

--- assets/db/schema.sql
+++ assets/db/schema.sql
@@ -3,10 +3,25 @@ create database employees_db;
 
 use employees_db;
 
-create table employees (
+create table department (
+    id int auto_increment primary key,
+    name varchar(30)
+);
+
+create table roles (
+    id int auto_increment primary key,
+    title varchar(30),
+    salary decimal,
+    department_id int,
+    foreign key (department_id) references department(id)
+)
+
+create table employee (
     id int not null auto_increment primary key,
     first_name varchar(30),
     last_name varchar(30),
-    role_id int not null,
-    manager_id int not null
+    role_id int,
+    manager_id int,
+    foreign key (role_id) references roles(id),
+    foreign key (manager_id) references employee(id)
 );
--- server.js
+++ server.js
@@ -21,3 +21,4 @@ const db = mysql.createConnection(
         database: 'employees_db'
     }
 );
+

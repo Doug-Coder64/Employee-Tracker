818b948eb15d50c0f96dfe82211c6a2c3e28f912
Doug Peterson <douglaspeterson6@gmail.com>
2021-10-24 18:50:17 -0500

    Added Query to view all employees

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
-const inquire = require('inquirer');
+const inquirer = require('inquirer');
 const mysql = require('mysql2');
-const ct = require('console.table');
+const cTable = require('console.table');
+const e = require('express');
 
 //database connection
 const db = mysql.createConnection(
     {
         host: 'localhost',
-        
+        port: 3306,
         //MySQL Username 
         user: 'root',
 
@@ -16,4 +17,53 @@ const db = mysql.createConnection(
     }
 );
 
-db.query
+function start() {
+    inquirer.prompt([
+        {
+            type: 'list',
+            message: 'What would you like to do?', 
+            name: "option", 
+            choices: [
+                    "View All Employees", 
+                    "View All Employees by Manager", 
+                    "View All Employees by Department",
+                    "Add Employee",
+                    "Remove Employee",
+                    "Update Employee Role",
+                    "Update Employee Manager"
+            ]
+        }
+    ]).then(function(result) {
+        
+        //Function is ran depending on selection
+        switch(result.option){
+            case "View All Employees":
+                console.log("Viewing employees");
+                viewAllEmployees();
+                break;
+            case "View All Employees by Manager":
+                break;
+            case "View All Employees by Department":
+                break;
+            case "Add Employee":
+                break;
+            case "Remove Employee":
+                break;
+            case "Update Employee Role":
+                break;
+            case "Update Employee Manager":
+                break;
+        }
+    })
+}
+
+function viewAllEmployees(){
+    
+    let query =  db.query(`SELECT employee.first_name, employee.last_name, role.title, role.salary, department.name, CONCAT(e.first_name, ' ' ,e.last_name) AS Manager FROM employee INNER JOIN role on role.id = employee.role_id INNER JOIN department on department.id = role.department_id left join employee e on employee.manager_id = e.id;`, function(err, res) {
+        if(err) throw err;
+        console.table(res);
+        start();
+    });
+}
+
+start();
